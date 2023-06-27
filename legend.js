fe73064d1ecd6c29bb22b80ca7629909b2b0f1b4
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2023-06-27 15:05:44 -0400

    fixed leyend color

--- legend.js
+++ legend.js
@@ -21,7 +21,7 @@ function Legend(color, {
     canvas.height = 1;
     const context = canvas.getContext("2d");
     for (let i = 0; i < n; ++i) {
-      context.fillStyle = color(i / (n - 1));
+      context.fillStyle = color(1 - i / (n - 1));
       context.fillRect(i, 0, 1, 1);
     }
     return canvas;
