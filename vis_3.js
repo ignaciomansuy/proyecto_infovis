8ed46de40f084661380d925d61388067ffb0ed2f
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2023-06-28 15:53:26 -0400

    fixed vis_3 animation and update

--- vis_3.js
+++ vis_3.js
@@ -11,14 +11,21 @@ const SVG3 = d3.select("#vis_3")
     .attr("transform",
           "translate(" + margin_3.left + "," + margin_3.top + ")");
 
+const xAxisG = SVG3.append("g")
+  .attr("transform", "translate(0," + height_3 + ")")
+  .attr("id", "xAxisG");
+
+const yAxisG = SVG3.append("g")
+  .attr("id", "yAxisG");
+
 // Parse the Data
 function vis3(data) {
   
   var x = d3.scaleLinear()
     .domain([0, d3.max(data, (d) => d.valor) * 1.1])
     .range([ 0, width_3]);
-  SVG3.append("g")
-    .attr("transform", "translate(0," + height_3 + ")")
+
+  d3.select('#xAxisG')
     .call(d3.axisBottom(x))
     .selectAll("text")
       .attr("transform", "translate(-10,0)rotate(-45)")
@@ -29,19 +36,25 @@ function vis3(data) {
     .range([ 0, height_3 ])
     .domain(data.map(function(d) { return d.causa; }))
     .padding(.5);
-  SVG3.append("g")
+  d3.select('#yAxisG')
     .call(d3.axisLeft(y))
 
   //Bars
-  SVG3.selectAll("myRect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", x(0) )
-    .attr("y", function(d) { return y(d.causa); })
-    .attr("width", function(d) { return x(d.valor); })
-    .attr("height", y.bandwidth() )
-    .attr("fill", "#69b3a2")
+  SVG3.selectAll(".bar")
+    .data(data, d => d.causa)
+    .join( enter => {
+      enter
+      .append("rect")
+      .attr("x", x(0) )
+      .attr("y", function(d) { return y(d.causa); })
+      .attr("height", y.bandwidth() )
+      .attr("fill", "#69b3a2")
+      .transition().duration(1000)
+      .attr("width", function(d) { return x(d.valor); })
+    },
+    update => update.transition().duration(1000).attr("width", function(d) { return x(d.valor); }),
+    exit => exit.remove()
+    )
 
 
     // .attr("x", function(d) { return x(d.causa); })
